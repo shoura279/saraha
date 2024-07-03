4598572d0c470f1de7f6a93bc70a5e000e0cdbb5
abdoww <abdow88988@gmaill.com>
2024-07-03 17:37:21 +0300

    add async handler & app error & global error handler

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import { connectDB } from './db/connection.js'
+import { globalErrorHandling } from './src/utils/asyncHandler.js'
 const app = express()
 const port = 3000
 
@@ -8,6 +9,7 @@ const port = 3000
 
 app.use(express.json())
 connectDB()
+app.use(globalErrorHandling)
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
