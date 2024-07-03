4920662609b20ae4eba2a35e09d1e21c72bc6621
YousefMohamed19 <yousef01121977646@gmail.com>
2024-07-03 17:41:37 +0300

    add send email

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ import { connectDB } from './db/connection.js'
 const app = express()
 const port = 3000
 
-
-
-
 app.use(express.json())
 connectDB()
+
+
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
